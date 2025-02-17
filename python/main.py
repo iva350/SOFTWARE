ffdea69c58e198ebcd7b4209efa1dae379bdbb7f
ivania <nathalyivaniamartinez@gmail.com>
2025-02-17 16:11:36 -0600

    actualizacion del codigo

--- python/main.py
+++ python/main.py
@@ -6,13 +6,13 @@ from user import User
 if __name__ == "__main__":
     print("Initializing car info")
     
-    driver = Driver("Miguel Rosales", "06760974-2", "chelemiguel@gmail.com", "chelito123")
+    driver = Driver("Ariela Guerrero", "06760974-3", "ariguerrero@gmail.com", "atex345")
     car = Car("P3N13F", driver)
     print(car)
     print(car.driver)
 
     print("Uber")
-    uber_driver = Driver("Miguel Rosales", "06760974-2", "chelemiguel@gmail.com", "chelito123")
+    uber_driver = Driver("Juan Carlos", "06760974-3", "carlosmartinez@gmail.com", "carlos123")
     uber = Uber("UBER39F1385", uber_driver, "Kia", "Soul")
     print(uber)
     print(uber.driver)
